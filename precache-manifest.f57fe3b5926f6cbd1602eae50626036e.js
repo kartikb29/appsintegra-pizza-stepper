3ad17d613a7e41d6921fb8facbab4885cb52108d
Kartikeya Bhargava <kartikeyabhargava95@gmail.com>
2019-11-26 06:36:53 +0400

    Updates

---
+++ precache-manifest.f57fe3b5926f6cbd1602eae50626036e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5019d0f9c4e9813dac2346ee7fb5eb27",
+    "url": "/appsintegra-pizza-stepper/index.html"
+  },
+  {
+    "revision": "ddd2d08974a4287b6e74",
+    "url": "/appsintegra-pizza-stepper/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "4dc526d76dfe6a4fab53",
+    "url": "/appsintegra-pizza-stepper/static/css/main.95648506.chunk.css"
+  },
+  {
+    "revision": "ddd2d08974a4287b6e74",
+    "url": "/appsintegra-pizza-stepper/static/js/2.b538d660.chunk.js"
+  },
+  {
+    "revision": "4dc526d76dfe6a4fab53",
+    "url": "/appsintegra-pizza-stepper/static/js/main.3862df07.chunk.js"
+  },
+  {
+    "revision": "ec057bb736f7aba757a5",
+    "url": "/appsintegra-pizza-stepper/static/js/runtime-main.997b0fb5.js"
+  }
+]);
